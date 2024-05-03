28f86f88214c1020a7e6f3b0fc9f4396ec9e89de
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-03 23:30:39 +0700

    fix: cookies not clear when login

--- controllers/authentications/Logout.js
+++ controllers/authentications/Logout.js
@@ -1,8 +1,8 @@
 const Logout = async (req, res) => {
     try {
 
-        res.clearCookie('refresh_token');
-        res.clearCookie('access_token');
+        res.clearCookie('refresh_token', { domain: process.env.MODE === 'production' ? '.posyayee.shop' : '.localhost', path: "/" });
+        res.clearCookie('access_token', { domain: process.env.MODE === 'production' ? '.posyayee.shop' : '.localhost', path: "/" });
 
         return res.status(200).json(
             {
