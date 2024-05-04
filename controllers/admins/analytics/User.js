9345a0b7b594fb1332e171a65dd6a7e0c908bd02
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-04 12:37:28 +0700

    refactor: added new validate pagination

--- controllers/admins/analytics/User.js
+++ controllers/admins/analytics/User.js
@@ -3,17 +3,25 @@ const Store = require("../../../models/Store");
 const User = require("../../../models/User");
 
 const getUser = async (req, res) => {
-    try {
 
+    try {
         const defaultSortBy = 'user_id';
         const allowedSortByAttributes = ['user_id', 'user_fname', 'user_lname', 'user_email', 'user_phone', 'user_role'];
-        const { page, perPage, sortBy, sort } = await validatePagination(req.query, allowedSortByAttributes, defaultSortBy);
+        let validated = await validatePagination(req.query, allowedSortByAttributes, defaultSortBy);
+
+        if (!validated) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid page or sort attributes!"
+            })
+        }
 
         const users = await User.findAndCountAll({
-            order: [[sortBy, sort.toUpperCase()]],
-            limit: perPage,
-            offset: (page - 1) * perPage,
+            limit: validated.perPage,
+            order: [[validated.sortBy, validated.sort]],
+            offset: (validated.page - 1) * validated.perPage,
             include: [{
+                model: Store,
                 attributes: ['store_id', 'store_name', 'store_address', 'store_remaining', 'store_active', 'store_taxid'],
             }],
             attributes: ['user_id', 'user_email', 'user_phone', 'user_image', 'user_accepted', 'user_acc_verify', 'user_active']
@@ -21,14 +29,22 @@ const getUser = async (req, res) => {
 
         return res.status(200).json({
             success: true,
-            page: page,
+            message: "Get users successfully!",
+            sort: validated.sort,
+            sortBy: validated.sortBy,
+            page: validated.page,
+            perPage: validated.perPage,
             total: users.count,
-            per_page: perPage,
             users: users.rows
         });
+
     } catch (error) {
         console.error("Error while fetching users", error);
-        return res.status(error.statusCode || 500).json({ error: error.message || 'Internal Server Error' });
+        return res.status(error.statusCode || 500).json(
+            {
+                success: false,
+                message: error.message || 'Internal Server Error'
+            });
     }
 };
 
