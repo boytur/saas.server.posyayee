0ed0e5e2637035bf17ce8097f06a2fd55ae25071
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2024-05-07 00:45:09 +0700

    feat(GetOrder): GetOpenOrder, GetOrder (GetOrderDetail not success yet!)

---
+++ controllers/payments/GetOrder.js
@@ -0,0 +1,102 @@
+const { getUserStoreId, getUserId } = require("../../libs/getUserData");
+const Order = require("../../models/Order");
+const { validatePagination } = require("../products/GetProduct");
+
+const GetOpenOrder = async (req, res) => {
+    try {
+
+        const storeId = await getUserStoreId(req);
+        const userId = await getUserId(req);
+
+        if (!storeId || !userId) {
+            return res.status(400).json({
+                message: "ร้านค้าหรือผู้ใช้งานไม่ถูกต้อง"
+            });
+        }
+
+        const order = await Order.findOne({
+            where: {
+                store_id: storeId,
+                user_id: userId,
+                order_status: 'open'
+            },
+            attributes: ['order_id', 'order_no', 'order_title', 'order_status', 'createdAt']
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Get open order successfully",
+            order: order
+        });
+    }
+    catch (e) {
+        console.error("Error getting order: ", e);
+    }
+}
+
+
+const GetOrder = async (req, res) => {
+    try {
+
+        const storeId = await getUserStoreId(req);
+        const userId = await getUserId(req);
+
+        const defaultSortBy = 'order_id';
+        const allowedSortByAttributes = ['order_id', 'order_no', 'order_title', 'order_price', 'order_status', 'createdAt'];
+
+        let validated = await validatePagination(req.query, allowedSortByAttributes, defaultSortBy);
+
+        if (!validated) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid page or sort attributes!"
+            })
+        }
+
+        if (!storeId || !userId) {
+            return res.status(400).json({
+                message: "ร้านค้าหรือผู้ใช้งานไม่ถูกต้อง"
+            });
+        }
+
+        const order = await Order.findAndCountAll({
+            order: [[validated.sortBy, validated.sort]],
+            limit: validated.perPage,
+            offset: (validated.page - 1) * validated.perPage,
+            where: {
+                store_id: storeId,
+                user_id: userId,
+            },
+            attributes: ['order_id', 'order_no', 'order_title', 'order_price', 'order_status', 'createdAt']
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "Get order successfully",
+            sort: validated.sort,
+            sortBy: validated.sortBy,
+            page: validated.page,
+            total: order.count,
+            per_page: validated.perPage,
+            order: order.rows
+        });
+    }
+    catch (e) {
+        console.error("Error getting order: ", e);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+const GetOrderDetail = async () => {
+    try {
+
+    }
+    catch (e) {
+        console.error("Error getting order detail: ", e);
+    }
+}
+
+module.exports = { GetOpenOrder, GetOrderDetail, GetOrder }
--- controllers/payments/Index.js
+++ controllers/payments/Index.js
@@ -3,9 +3,14 @@ const VerifyPayment = require('./VerifyPayment');
 const bodyParser = require('body-parser');
 const GetOrderSessionId = require('./GetOrderSessionId');
 const { can_get_order_session_id } = require('../../middlewares/permission');
+const { GetOrderDetail, GetOpenOrder, GetOrder } = require('./GetOrder');
 const payments = express.Router();
 
 payments.post('/api/payment/webhook', bodyParser.raw({ type: 'application/json' }), VerifyPayment);
 payments.get('/api/payment/get-payment-order/:order_id', can_get_order_session_id, GetOrderSessionId);
+
+payments.get('/api/payment/get-open-order', can_get_order_session_id, GetOpenOrder);
+payments.get('/api/payment/get-order', can_get_order_session_id, GetOrder);
+payments.get('/api/payment/get-order/:order_id', can_get_order_session_id, GetOrderDetail);
 
 module.exports = payments;
