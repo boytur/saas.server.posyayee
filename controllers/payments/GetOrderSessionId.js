802121bf3f93d57b6b6a60710aebd2f50cdd1190
boytur <mask30254@gmail.com>
2024-05-12 08:27:33 +0700

    feat(GetOrderSessionId): added function to handle exprired checkout session

--- controllers/payments/GetOrderSessionId.js
+++ controllers/payments/GetOrderSessionId.js
@@ -1,7 +1,8 @@
-const { where } = require("sequelize");
 const Order = require("../../models/Order");
 const { validateInteger } = require("../../libs/validate");
 const { getUserStoreId } = require("../../libs/getUserData");
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const GetOrderSessionId = async (req, res) => {
     try {
@@ -31,6 +32,65 @@ const GetOrderSessionId = async (req, res) => {
             });
         }
 
+        // get old session expires time
+        const oldSession = await stripe.checkout.sessions.retrieve(order.order_session_id);
+        const expires_at = oldSession.expires_at;
+        const expirationTime = new Date(expires_at * 1000);
+        const currentTime = new Date();
+
+        // if expires order and create new order using old data
+        if (expirationTime < currentTime) {
+
+            console.log('======== expired order ==============');
+
+            // update order 
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ['card', 'promptpay'],
+                line_items: [{
+                    price_data: {
+                        currency: 'thb',
+                        product_data: {
+                            name: order.order_title,
+                        },
+                        unit_amount: `${(order.order_price) * (100)}`,
+                    },
+                    quantity: 1,
+                }],
+                mode: 'payment',
+                success_url: `${process.env.MODE === "production" ? "https://posyayee.shop" : "http://localhost:5173"}` + `/payment/success?id=${order.order_id_no}`,
+            });
+
+            if (!session) {
+                throw new Error("เกิดข้อผิดพลาดระหว่างการสร้างรายการออเดอร์");
+            }
+
+            const updateOrder = await Order.update(
+                { order_session_id: session.id },
+                { where: { order_id: order.order_id } }
+            );
+
+            console.log('======== update expired order =========');
+            console.log(updateOrder);
+
+            return res.status(200).json({
+                order: {
+                    order_id: order.order_id,
+                    order_no: order.order_no,
+                    order_title: order.order_title,
+                    order_note: order.order_note,
+                    order_status: order.order_status,
+                    order_price: order.order_price,
+                    order_session_id: order.order_session_id,
+                    ordder_expires_at: session.expires_at,
+                },
+                user: {
+                    user_id: order.user_id,
+                    package_id: order.package_id,
+                    store_id: order.store_id,
+                }
+            });
+        }
+
         return res.status(200).json({
             order: {
                 order_id: order.order_id,
@@ -39,7 +99,8 @@ const GetOrderSessionId = async (req, res) => {
                 order_note: order.order_note,
                 order_status: order.order_status,
                 order_price: order.order_price,
-                order_session_id: order.order_session_id
+                order_session_id: order.order_session_id,
+                ordder_expires_at: expirationTime,
             },
             user: {
                 user_id: order.user_id,
@@ -47,6 +108,7 @@ const GetOrderSessionId = async (req, res) => {
                 store_id: order.store_id,
             }
         });
+
     } catch (err) {
         console.error("Err while get order session id: ", err);
         return res.status(500).json({
